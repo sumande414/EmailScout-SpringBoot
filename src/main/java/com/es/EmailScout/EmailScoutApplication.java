177dd5b01ab4f17c4b2951988e54fdc6861dc1a6
Suman De <sumande414@gmail.com>
2025-02-20 21:28:32 +0530

    minor changes

--- src/main/java/com/es/EmailScout/EmailScoutApplication.java
+++ src/main/java/com/es/EmailScout/EmailScoutApplication.java
@@ -7,14 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmailScoutApplication {
 	static {
-    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-    dotenv.entries().forEach(entry -> {
-        // Set System property only if it's not already set (i.e., Railway env should take priority)
-        if (System.getenv(entry.getKey()) == null) {
-            System.setProperty(entry.getKey(), entry.getValue());
-        }
-    });
-}
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> {
+            if (System.getenv(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(EmailScoutApplication.class, args);
 	}
